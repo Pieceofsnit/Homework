68991fcdd4c81519d79bc27ed3e39eebb28e4635
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2023-04-26 22:08:22 +0500

    Update EntryZone.cs

--- Assets/Scripts/EntryZone.cs
+++ Assets/Scripts/EntryZone.cs
@@ -7,17 +7,15 @@ public class EntryZone : MonoBehaviour
     [SerializeField] public float recoveryRate;
 
     public Alarm _alarm;
-    private static float _currValue;
+    private float _currValue;
     private float _targetValue;
     private float _maxVolume = 1f;
     private float _minVolume = 0f;
-
 
     private IEnumerator ChangeVolumeSound()
     {
         while (_targetValue != _currValue)
         {
-            Debug.Log("Worked");
             _currValue = Mathf.MoveTowards(_currValue, _targetValue, recoveryRate * Time.deltaTime);
             _alarm.ChangeVolume(_currValue);
             yield return null;
@@ -26,9 +24,7 @@ public class EntryZone : MonoBehaviour
         if (_currValue == _minVolume)
         {
             _alarm.StopSound();
-            Debug.Log("Stop");
         }
-
     }
 
     private void OnTriggerEnter(Collider colision)
@@ -38,14 +34,12 @@ public class EntryZone : MonoBehaviour
             _targetValue = _maxVolume;
             _alarm.PlaySound();
             StartCoroutine(ChangeVolumeSound());
-            Debug.Log("Play");
         }
     }
 
     private void OnTriggerExit(Collider colision)
     {
         _targetValue = _minVolume;
-
         StartCoroutine(ChangeVolumeSound());
     }
 }
