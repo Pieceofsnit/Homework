a9918b63d74f60efa48821418e7556788f40932c
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2023-04-28 12:33:19 +0500

    Alarm, EntryZone
    changed folder location "Scripts"
    change code

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private float _recoveryRate;
+
+    private float _maxVolume = 1f;
+    private float _minVolume = 0f;
+    private float _targetValue;
+    private float _currValue;
+
+    public void PlaySound()
+    {
+        _targetValue = _maxVolume;
+        _audioSource.Play();
+    }
+
+    public void StopSound()
+    {
+        _audioSource.Stop();
+    }
+
+    public IEnumerator ChangeVolumeSound()
+    {
+        while (_targetValue != _currValue)
+        {
+            Debug.Log("Worked");
+            _currValue = Mathf.MoveTowards(_currValue, _targetValue, _recoveryRate * Time.deltaTime);
+            _audioSource.volume = _currValue;
+            yield return null;
+        }
+
+        _targetValue = _minVolume;
+
+        if (_currValue == _minVolume)
+        {
+            StopSound();
+            Debug.Log("Stop");
+        }
+    }
+}
---
+++ Assets/Scripts/EntryZone.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntryZone : MonoBehaviour
+{
+    [SerializeField] private Alarm _alarm;
+
+    private void OnTriggerEnter(Collider colision)
+    {
+        if (colision.TryGetComponent<Player>(out Player player))
+        {
+            _alarm.PlaySound();
+            StartCoroutine(_alarm.ChangeVolumeSound());
+            Debug.Log("Play");
+        }
+    }
+
+    private void OnTriggerExit(Collider colision)
+    {
+        StartCoroutine(_alarm.ChangeVolumeSound());
+    }
+}
