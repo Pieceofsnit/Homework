5cfbd91c0617eef5622bc802d7083ae0e7274df4
Pieceofsnit <102411114+Pieceofsnit@users.noreply.github.com>
2023-05-03 12:18:24 +0500

    update Alarm, EntryZone

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -7,37 +7,45 @@ public class Alarm : MonoBehaviour
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private float _recoveryRate;
 
-    private float _maxVolume = 1f;
+    private Coroutine _volumeChange;
     private float _minVolume = 0f;
     private float _targetValue;
     private float _currValue;
 
-    public void PlaySound()
+    private void PlaySound()
     {
-        _targetValue = _maxVolume;
         _audioSource.Play();
     }
 
-    public void StopSound()
+    private void StopSound()
     {
-        _targetValue = _minVolume;
         _audioSource.Stop();
     }
 
-    private IEnumerator ChangeVolumeSound()
+    public void TurnVolume(float targetVolume)
     {
-        StartCoroutine(ChangeVolumeSound);
+        _targetValue = targetVolume;
+
+        if (_volumeChange != null)
+        {
+            StopCoroutine(_volumeChange);
+        } 
+        _volumeChange = StartCoroutine(ChangeVolumeSound());
+    }
+
+    public IEnumerator ChangeVolumeSound()
+    {
+        if (_targetValue > _minVolume)
+            PlaySound();
 
         while (_targetValue != _currValue)
         {
             _currValue = Mathf.MoveTowards(_currValue, _targetValue, _recoveryRate * Time.deltaTime);
             _audioSource.volume = _currValue;
             yield return null;
-        }
-
-        if (_currValue == _minVolume)
-        {
-            StopSound();
+            
+            if(_currValue == _minVolume)
+                StopSound();
         }
     }
 }
--- Assets/Scripts/EntryZone.cs
+++ Assets/Scripts/EntryZone.cs
@@ -6,17 +6,19 @@ public class EntryZone : MonoBehaviour
 {
     [SerializeField] private Alarm _alarm;
 
+    private float _maxVolume = 1f;
+    private float _minVolume = 0f;
+
     private void OnTriggerEnter(Collider colision)
     {
         if (colision.TryGetComponent<Player>(out Player player))
         {
-            _alarm.ChangeVolumeSound();
-            _alarm.PlaySound();
+            _alarm.TurnVolume(_maxVolume);
         }
     }
 
     private void OnTriggerExit(Collider colision)
     {
-        _alarm.ChangeVolumeSound();
+        _alarm.TurnVolume(_minVolume);
     }
 }
